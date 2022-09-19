f938eacedd11e9bebee8275c95b65ab99eff8ecb
Omolola Odunowo <omololaodunowo@yahoo.com>
2022-09-19 21:36:15 +0100

    first task corrected

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 /**
  * void prototypes()
  * int prototypes()
  */
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int_strlen(char *str);
@@ -16,6 +18,5 @@ void puts2(char *s);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *str);
 
 #endif /*MAIN_H*/
