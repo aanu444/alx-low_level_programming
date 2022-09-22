4d82b0b0434b82f3cf8a39f148afbc898ca3b4d1
Omolola Odunowo <omololaodunowo@yahoo.com>
2022-09-22 21:46:48 +0100

    hot fix--task six

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,7 @@
 
 char *cap_string(char *str)
 {
-	char spc[] = {32, 9, '\n', '', ';', '.', '!', '?', '"', "(", ")", "{", "}"};
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', "(", ")", "{", "}"};
 	int len = 13;
 	int a = 0, i;
 
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || str[a - 1] == spc [i]) && (str[a] >= 97 && str[a] <= 122))
+			if ((a == 0 || str[a - 1] == spc[i]) && (str[a] >= 97 && str[a] <= 122))
 				str[a] = str[a] - 32;
 			i++;
 		}
