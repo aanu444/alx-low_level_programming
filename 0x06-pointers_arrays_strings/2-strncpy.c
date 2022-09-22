97837aefa2afe085229b0c2d2556a97d20592fbc
Omolola Odunowo <omololaodunowo@yahoo.com>
2022-09-22 15:31:16 +0100

    hot fix-- task two done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strncpy - Copies at most an inputted number of bytes
  * @dest: the buffer storing the string copy
@@ -9,16 +9,6 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0; src_len = 0;
-
-	while (src[index++])
-		src_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
-
-	return (dest);
+	strncpy(dest, src, n);
+	return (dest)
 }
